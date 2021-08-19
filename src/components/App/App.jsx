5c43d3c89f6d8977baed03136ad108af25fc6c12
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-08-19 11:03:41 -0500

    inital commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
 function App() {
-
+  
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Prime Pizza</h1>
       </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/order">ORDER</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact>
+        <img src="images/pizza_photo.png" />
+        <p>Pizza is great.</p>
+        </Route>
+        <Route path="/order" exact>
+          {/* Enter Select pizza page */}
+        </Route>
+        <Route path="/customer" exact>
+          {/* Enter Select pizza page */}
+        </Route>
+        <Route path="/checkout" exact>
+          {/* Enter Select pizza page */}
+        </Route>
+      </Router>
     </div>
   );
 }
